a8a00b1a00cb97a8ce0f4e71c1849435f3598864
Zk <zklim95@hotmail.com>
2023-02-16 23:07:34 +0800

    Uncaught ReferenceError: process is not defined

---
+++ client/src/circuits/eddsa.js
@@ -0,0 +1,66 @@
+import { buildEddsa, buildBabyjub } from "circomlibjs";
+import { arrayify } from "ethers/lib/utils";
+import { BigNumber } from "ethers";
+
+export class EdDSA {
+  _eddsa;
+  _babyjub;
+  _prvKey;
+  _pubKey;
+
+  get initialized() {
+    return (
+      this._eddsa !== undefined &&
+      this._babyjub !== undefined &&
+      this._prvKey !== undefined &&
+      this._pubKey !== undefined
+    );
+  }
+
+  get pubKey() {
+    if (!this._pubKey) throw Error("Not initialized");
+    return this._pubKey;
+  }
+
+  get babyjub() {
+    if (!this._babyjub) throw Error("Not initialized");
+    return this._babyjub;
+  }
+
+  get scalarPubKey() {
+    if (!this._pubKey) throw Error("Not initialized");
+    return [
+      this._babyjub.F.toObject(this._pubKey[0]),
+      this._babyjub.F.toObject(this._pubKey[1]),
+    ];
+  }
+
+  constructor(privKey) {
+    this._prvKey = arrayify(privKey);
+  }
+
+  async init() {
+    if (this.initialized) return this;
+    this._eddsa = await buildEddsa();
+    this._babyjub = await buildBabyjub();
+    this._pubKey = await this._eddsa.prv2pub(this._prvKey);
+    return this;
+  }
+
+  async sign(message) {
+    const m = this._babyjub.F.e(BigNumber.from(message).toString());
+    const signature = this._eddsa.signPoseidon(this._prvKey, m);
+    if (!this.verify(m, signature, this.pubKey))
+      throw Error("generated invalid eddsa signature");
+    return signature;
+  }
+
+  async verify(
+    message,
+    signature,
+    pubKey
+  ) {
+    const m = this._babyjub.F.e(BigNumber.from(message).toString());
+    return this._eddsa.verifyPoseidon(m, signature, pubKey);
+  }
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useEthers } from "@usedapp/core";
+import { BigNumber } from "ethers";
+import SignEdDSA from "../components/SignEdDSA";
+
+const msgToSign = BigNumber.from("0x1234").toBigInt();
 
 const Home = () => {
+	const { account } = useEthers();
+	const privKey = account;
+
 	return (
 		<>
 			<div className="text-[20px] font-bold">Welcome to SafeHome</div>
+			<h2>Get Signature</h2>
+			<SignEdDSA
+        	privKey={privKey}
+        	message={msgToSign}
+     		onResult={(result) => setEdDSASignature(result)}
+      		/>
 		</>
 	);
 };
