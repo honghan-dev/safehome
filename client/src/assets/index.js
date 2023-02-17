4894f3d701b54ff9b591b2dbea8b23e7308583a3
Lim Hong Han <limhonghan1987@gmail.com>
2023-02-17 19:36:06 +0800

    Added qrcode functions

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,8 @@
 import loader from "./loader.svg";
 import logo from "./logo.png";
 import hello from "./hello.svg";
+import qrcode from "./qrcode.png";
 import relax from "./relax.svg";
 import security from "./security.svg";
 
-export { loader, logo, hello, relax, security };
+export { loader, logo, hello, relax, qrcode, security };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,52 @@
-import React from "react";
-import { hello } from "./../assets";
-
-import QRCode from "qrcode";
+import React, { useState } from "react";
+import { hello, qrcode } from "./../assets";
+import Modal from "react-modal";
 
 const Home = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const customStyles = {
+		content: {
+			top: "50%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)",
+			borderRadius: "12px",
+			width: "70vw",
+			height: "70vh",
+		},
+	};
+
 	return (
 		<div className="px-[24px]">
 			<div className="text-[20px] font-bold">Welcome to SafeHome</div>
-			<div className="flex w-full justify-center mt-[20px]">
+			<div className="flex w-full flex-col justify-center mt-[20px]">
 				<img
 					src={hello}
 					alt="security"
 					className="w-3/4"
 				/>
+				<div className="flex justify-center">
+					<button
+						className="font-epilogue w-2/3 text-white text-[16px] bg-[#0052B6] px-2 py-2 rounded-[10px] mt-8"
+						onClick={() => setIsOpen(true)}
+					>
+						Show QR Code
+					</button>
+				</div>
+				<Modal
+					isOpen={isOpen}
+					onRequestClose={() => setIsOpen(false)}
+					ariaHideApp={false}
+					style={customStyles}
+				>
+					<img
+						src={qrcode}
+						alt="qr code"
+					/>
+				</Modal>
 			</div>
 		</div>
 	);
